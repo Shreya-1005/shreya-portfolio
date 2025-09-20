a3e915e5239e7da144e5ae8e1a31b03ae65df9c3
Shreya Srivastava <shreeja.srivastava2005@gmail.com>
2025-09-20 17:28:37 +0530

    Correcting the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
-import Projects from './components/projects.jsx';
-import Skills from './components/skills.jsx';
+import Projects from './components/Projects.jsx';
+import Skills from './components/Skills.jsx';
 import Contact from './components/Contact.jsx';
 import './App.css';
 
